f39c3d054b29f0fdd0a4a7f448e687a9a3a77a69
dario <dario.faudella@mail.polimi.it>
2021-01-22 11:14:38 +0100

    drag draw works

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,37 @@
-function preload() {
-  // put preload code here
+let socket = io();
+
+socket.on("connect", newConnection);
+
+function newConnection() {
+  console.log("your id:", socket.id);
 }
+
+socket.on("mouseBroadcast", otherMouse);
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background("red");
 }
 
-function draw() {
-  ellipse(mouseX, mouseY, 30);
+function otherMouse(data) {
+  console.log("received:", data);
+  noStroke();
+  fill("yellow");
+  ellipse(data.x, data.y, 20);
 }
+
+function mouseDragged() {
+  console.log("sending: ", mouseX, mouseY);
+  noStroke();
+  fill(255);
+
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+  socket.emit("mouse", message);
+
+  ellipse(mouseX, mouseY, 20);
+}
+
+function draw() {}
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-console.log("up and running");
-
-let express = require('express')
-
+let express = require("express");
 let app = express();
-
-let port = 3000;
-
+let port = process.env.PORT || 3000;
 let server = app.listen(port);
+app.use(express.static("public"));
+let socket = require("socket.io");
+let io = socket(server);
+io.on("connection", newConnection);
 
-app.use(express.static('public'));
+function newConnection(socket) {
+  console.log("socket:", socket.id);
+
+  socket.on("mouse", mouseMessage);
+
+  function mouseMessage(data) {
+    socket.broadcast.emit("mouseBroadcast", data);
+    console.log(socket.id, data);
+  }
+}
+
+console.log("node server is running");
